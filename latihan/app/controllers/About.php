67f3deff4f4c34423e2e24f89d0b689b65304c53
Camela Devs <camelaputri1@gmail.com>
2023-09-17 18:50:29 +0700

    View

--- latihan/app/controllers/About.php
+++ latihan/app/controllers/About.php
@@ -1,12 +1,23 @@
 <?php 
 
-class About {
-    public function index($nama = 'Kucing', $pekerjaan = 'Tidur')
+class About extends Controller {
+    public function index($nama = 'Admin', $status = 'Admin', $alamat = 'Admin')
     {
-        echo "Halo $nama, job saya $pekerjaan";
+        $data['judul'] = 'About';
+        $data['nama'] = $nama;
+        $data['status'] = $status;
+        $data['alamat'] = $alamat;
+
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
     public function page()
     {
-        echo 'About/page';
+        $data['judul'] = 'Page';
+        
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
---
+++ latihan/app/views/about/index.php
@@ -0,0 +1,5 @@
+
+    <h1>Halaman about</h1>
+    <p>Nama: <?= $data['nama']; ?></p>
+    <p>Status: <?= $data['status']; ?></p>
+    <p>Alamat: <?= $data['alamat']; ?></p>
